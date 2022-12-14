faf42fb07fb9ed1dd4fd3731f985c0555c9dfc35
Sefa <sefak.arslan96@gmail.com>
2022-12-14 16:58:33 +0300

    Add project files.

---
+++ RealWorldUnitTest.Web/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealWorldUnitTest.Web.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+
+        Task<TEntity> GetById(int id);
+
+        Task Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
